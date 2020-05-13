e39019b903fd2d277589eb317a2fa2da5f04ecab
Donastien <donastien.karoum@gmail.com>
2020-05-13 16:57:48 +0200

    Initialisation Server & Schema

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema');
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
