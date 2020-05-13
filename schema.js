081f2ee0ae40bc28b48f73d8da2a0cc277f80d24
Donastien <donastien.karoum@gmail.com>
2020-05-13 17:04:06 +0200

    Root Query

--- schema.js
+++ schema.js
@@ -1,8 +1,11 @@
+const axios = require('axios');
 const {
   GraphQLObjectType,
   GraphQLInt,
   GraphQLString,
   GraphQLBoolean,
+  GraphQLList,
+  GraphQLSchema,
 } = require('graphql');
 
 // Launch Type
@@ -26,4 +29,23 @@ const RocketType = new GraphQLObjectType({
     rocket_name: { type: GraphQLString },
     rocket_type: { type: GraphQLString },
   }),
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    launches: {
+      type: new GraphQLList(LaunchType),
+      resolve(parent, args) {
+        return axios
+          .get('https://api.spacexdata.com/v3/launches')
+          .then((res = res.data));
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
