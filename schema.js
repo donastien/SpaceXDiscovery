3e4c666ab45a2ca365d3086b24b863016ac1453a
Donastien <donastien.karoum@gmail.com>
2020-05-13 17:32:07 +0200

    Change ID rocket

--- schema.js
+++ schema.js
@@ -54,6 +54,25 @@ const RootQuery = new GraphQLObjectType({
           .then((res) => res.data);
       },
     },
+    rockets: {
+      type: new GraphQLList(RocketType),
+      resolve(parent, args) {
+        return axios
+          .get('https://api.spacexdata.com/v3/rockets')
+          .then((res) => res.data);
+      },
+    },
+    rocket: {
+      type: RocketType,
+      args: {
+        rocket_id: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        return axios
+          .get(`https://api.spacexdata.com/v3/rockets/${args.rocket_id}`)
+          .then((res) => res.data);
+      },
+    },
   },
 });
 
