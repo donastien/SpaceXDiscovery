075b021b95e43e1dfb3f9595f472f61a1ccc1396
Donastien <donastien.karoum@gmail.com>
2020-05-19 18:21:43 +0200

    Positionnement de falcon1

--- client/src/components/Rockets.js
+++ client/src/components/Rockets.js
@@ -7,26 +7,26 @@ import starships from '../img/starships.png';
 const Rockets = () => {
   return (
     <section class='rockets' id='rockets'>
-      <div class='container-fluid'>
+      <div class='container'>
         <div class='section-heading text-light text-center'>
           <h2>Rockets</h2>
-          <p class='text-muted'>Voici les célèbres fusées de Space X.</p>
+          <p class='text-muted'>Les célèbres fusées Space X.</p>
 
           <hr />
         </div>
         <div className='row'>
-          <div className='col-lg-3 border text-center'>
-            <img src={falcon1} class='img-fluid' alt='' />
+          <div className='col-lg-3 text-center'>
+            <img src={falcon1} class='falcon1 img-fluid' alt='' />
           </div>
-          <div className='col-lg-3 border text-center'>
+          <div className='col-lg-3 text-center'>
             <img src={falcon9} class='img-fluid' alt='' />
           </div>
 
-          <div className='col-lg-3 border'>
+          <div className='col-lg-3 text-center'>
             <img src={falconheavy} class='img-fluid' alt='' />
           </div>
-          <div className='col-lg-3 border'>
-            <img src={starships} class='img-fluid' alt='' />
+          <div className='col-lg-3 text-center border'>
+            <img src={starships} class='starships img-fluid' alt='' />
           </div>
         </div>
       </div>
