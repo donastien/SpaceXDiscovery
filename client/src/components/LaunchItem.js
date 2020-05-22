c56d66689296f7298bfc426b8dfcbeec5798327d
Donastien <donastien.karoum@gmail.com>
2020-05-22 17:14:42 +0200

    Link Launch

--- client/src/components/LaunchItem.js
+++ client/src/components/LaunchItem.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import classNames from 'classnames';
 import Moment from 'react-moment';
+import { Link } from 'react-router-dom';
 
 const LaunchItem = ({
-  launch: { flight_number, mission_name, launch_date_local, launch_success },
+  launch: {
+    flight_number,
+    mission_name,
+    launch_date_local,
+    launch_success,
+    rocket: { rocket_name },
+  },
 }) => {
   return (
     <tr>
       <td>{flight_number}</td>
 
-      <td>{mission_name}</td>
+      <td>
+        <Link to={`/launch/${flight_number}`}>{mission_name}</Link>
+      </td>
 
-      <td>rocket name</td>
+      <td>{rocket_name}</td>
       <td>
         {' '}
         <Moment format='DD MMMM YYYY HH:mm'>{launch_date_local}</Moment>
